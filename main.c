be9493492f362c523543d3d1178e98ca7913c153
andronix1 <andronix1.dev@gmail.com>
2024-10-06 16:10:25 +0700

    some refactoring

--- main.c
+++ main.c
@@ -22,6 +22,9 @@ expected_result_t results[] = {
 };
 #define DATA_LENGTH (sizeof(results) / sizeof(expected_result_t))
 
+#define TRAIN_EPOCHS 1000000
+#define PRINT_EPOCHS 10
+
 int main() {
 	srand(100);
 	train_sample_t sample = {
@@ -33,10 +36,10 @@ int main() {
         neuron_init(&neuron, 2, sigmoidf);
 	neuron_random(&neuron);
 	
-	for (size_t i = 0; i < 1000000; i++) {
+	for (size_t i = 0; i < TRAIN_EPOCHS; i++) {
 		neuron_train_epoch(&neuron, &sample, 1e-1);
 		float cost = neuron_cost(&neuron, &sample);
-		if (i % 100000 == 0) {
+		if (i % (TRAIN_EPOCHS / PRINT_EPOCHS) == 0) {
 			printf("epoch %d: cost = %f\n", i, cost);
 		}
 	}
